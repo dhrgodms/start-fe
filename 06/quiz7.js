9d65b2ec69a19d9176fc3122fdd5c825f26ca033
dhrgodms <dhrgodms123@gmail.com>
2023-04-13 16:21:37 +0900

    bug : life 감소하고 다음게임 진행 안되는 bug 해결

--- 06/quiz7.js
+++ 06/quiz7.js
@@ -3,16 +3,22 @@ const $bug = document.querySelector("#bug");
 const $point = document.querySelector("#point");
 const $life = document.querySelector("#life");
 
-$bug.addEventListener("click", (e) => {
+let pointNumber = 0;
+let lifeNumber = 10;
+
+$box.addEventListener("click", (e) => {
   let randomNum = Math.floor(Math.random() * 390) + 1;
   $bug.style.left = randomNum + "px";
   $bug.style.top = randomNum + "px";
-});
 
-$box.addEventListener("click", (e) => {
   if (e.target === $bug) {
-    $point.textContent = parseInt($point.textContent) + 1;
+    pointNumber += 1;
+    $point.textContent = pointNumber;
   } else {
-    $life.textContent = parseInt($life.textContent) - 1;
+    lifeNumber -= 1;
+    $life.textContent = lifeNumber;
   }
 });
+
+// dom에서 조작할 수 있을 경우,
+// 사용자가 조작할 수 있으므로 백엔드에서 값을 체크해야한다.
